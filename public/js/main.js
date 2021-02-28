3dcc67b32756d4f0acfe0f6168fa78ab41beb09f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-02-28 16:38:46 +0200

    Message Emit and Catch from Client to Server

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,33 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 socket.on('message', async (message) => {
   console.log(message);
+});
+
+socket.on('leftMessage', async (message) => {
+  console.log(message);
+});
+
+socket.on('user', async (data) => {
+  console.log(data);
+});
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //Get message text from input element under the form
+  const message = e.target.elements.msg.value;
+
+  // const data = {
+  //   name: 'Aytac',
+  //   surname: 'Guley',
+  //   age: 41,
+  // };
+
+  //Emit message to server
+  //socket.emit('chatMessage', data);
+
+  socket.emit('chatMessage', message);
 });
--- server.js
+++ server.js
@@ -18,6 +18,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Run when client connects
 io.on('connection', (socket) => {
   console.log('New ws Connection...');
+  const data = {
+    name: 'Aytac',
+    surname: 'Güley',
+    age: 41,
+  };
+
+  socket.emit('user', data);
+  //This will send the message who connected to the server
+  socket.emit('message', 'Welcome to ChatCord');
+
+  //broadcast when a user connects (this will send everybody except the one who newly connected to the server and fired this function);
+  socket.broadcast.emit('message', 'A user has joined the chat');
+
+  //When a user disconnects, we will use :
+  socket.on('disconnect', () => {
+    io.emit('leftMessage', 'A User has left the chat');
+  });
+
+  socket.on('chatMessage', (msg) => {
+    console.log(msg);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
